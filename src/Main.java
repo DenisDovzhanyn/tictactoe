7bd9f174ad97c933fa91d299a860dd5d1dabb5e2
Denis <dovzhanyndenis03@gmail.com>
2024-05-17 23:02:51 -0400

    done

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
-    char[][] board = new char[3][3];
+     char[][] board = new char[3][3];
 
     static boolean isPlayer1 = true;
-    static int score;
+
+    static int player1Score;
+    static int player2Score;
 
 
     public static void main(String[] args) {
@@ -21,10 +23,18 @@ public class Main {
             game.getPlayerInput(input);
 
             game.printBoard();
+            if (game.didYouWin() == true){
+                System.out.println("Player ones score: " + player1Score);
+                System.out.println("Player twos score: " + player2Score);
+                break;
+            }
             isPlayer1 = !isPlayer1;
+
+            if (turn == 8 && game.didYouWin() == false){
+                System.out.println("No one wins!");
+            }
+
         }
-
-
 
     }
 
@@ -50,11 +60,11 @@ public class Main {
 
 
             // checks to see if a player has claimed the spot, otherwise will loop until un-unclaimed spot is chosen
-            if (getBoard(row, col) != '-') {
+            if (board[row][col] != '-') {
                 System.out.println("A player already has claimed that spot!");
             }
 
-        } while (getBoard(row, col) != '-');
+        } while (board[row][col] != '-');
 
         setBoard(row, col);
     }
@@ -94,9 +104,7 @@ public class Main {
 
     }
 
-    public char getBoard(int row, int col){
-        return board[row][col];
-    }
+
 
     public String whoTurn(){
         // checks to see whos turn it is
@@ -105,6 +113,62 @@ public class Main {
         } else {
             return  "Player twos turn";
         }
+    }
+
+    public boolean didYouWin(){
+        for (int a = 0; a < board.length ; a++){
+            if (board[a][0] == board[a][1] && board[a][1] == board[a][2] && board[a][0] != '-'){
+                if (board[a][0] == 'x'){
+                    player1Wins();
+                    return true;
+                }else {
+                    player2Wins();
+                    return true;
+                }
+            }
+        }
+
+        for (int b = 0; b < board.length ; b++){
+            if (board[0][b] == board[1][b] && board[1][b] == board[2][b] && board[0][b] != '-'){
+                if (board[0][b] == 'x'){
+                    player1Wins();
+                    return true;
+                } else {
+                    player2Wins();
+                    return true;
+                }
+            }
+        }
+
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != '-') {
+            if (board[0][0] == 'x') {
+                    player1Wins();
+                    return true;
+            } else {
+                    player2Wins();
+                    return true;
+            }
+        }
+
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '-'){
+            if (board[0][2] == 'x') {
+                player1Wins();
+                return true;
+            } else {
+                player2Wins();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void player1Wins(){
+        System.out.println("Player one wins!");
+        player1Score++;
+    }
+    public void player2Wins(){
+        System.out.println("Player two wins!");
+        player2Score++;
     }
 
 }
