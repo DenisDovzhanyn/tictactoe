7e508011a7ab29e8b8439b3264439c2e669697e2
Denis <dovzhanyndenis03@gmail.com>
2024-05-17 21:31:25 -0400

    stuck on if else in do while loop

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
-     int boardLength = 3;
-    char[][] board = new char[boardLength][boardLength];
+    char[][] board = new char[3][3];
     int row;
     int col;
-    char x = 'x';
-    char o = 'o';
     static boolean isPlayer1 = true;
+    static int score;
 
 
     public static void main(String[] args) {
@@ -20,29 +18,46 @@ public class Main {
         for (int turn = 0; turn < 9 ; turn++){
             System.out.println("It is currently " + game.whoTurn());
             System.out.println("Please choose a row from 1 to 3");
-            game.setRow(input);
 
-            // checks to see if input is within bounds, if it is, will continue running without problems
-            while (game.getRow() < 1  || game.getRow() > 3){
-                System.out.println("That is not between 1 and 3! Please input a correct row");
+            do {
+                // checks to see if input is out of bounds, if it is, will run while loop
                 game.setRow(input);
-            }
+                while (game.getRow() < 0 || game.getRow() > 2) {
+                    System.out.println("That is not between 1 and 3! Please input a correct row");
+                    game.setRow(input);
+                }
 
-            System.out.println("Now please choose a column");
-            game.setCol(input);
-            while (game.getCol() < 1 || game.getCol() > 3){
-                System.out.println("That is not between 1 and 3! Please input a correct column");
+                System.out.println("Now please choose a column");
                 game.setCol(input);
-            }
+                while (game.getCol() < 0 || game.getCol() > 2) {
+                    System.out.println("That is not between 1 and 3! Please input a correct column");
+                    game.setCol(input);
+                }
 
-            game.setBoard(game.getRow() - 1, game.getCol() - 1);
+                if (game.getBoard(game.getRow(), game.getCol()) != '-') {
+                    System.out.println("A player already has claimed that spot!");
+                } else {
+                    game.setBoard(game.getRow(), game.getCol());
+
+                }
+            } while (game.getBoard(game.getRow(), game.getCol()) != '-');
+
+            /*checks to see if the spot is already taken, if the spot is taken then the loop keeps running until an unclaimed spot is input
+            do {
+                if (game.getBoard(game.getRow(), game.getCol()) != '-') {
+                    System.out.println("A player already has claimed that spot!");
+                } else {
+                    game.setBoard(game.getRow(), game.getCol());
+
+                }
+            } while (game.getBoard(game.getRow(), game.getCol()) != '-');
+
+             */
+
             game.printBoard();
 
-            if (isPlayer1 == true){
-                isPlayer1 = false;
-            } else {
-                isPlayer1 = true;
-            }
+
+            isPlayer1 = !isPlayer1;
 
 
 
@@ -55,7 +70,7 @@ public class Main {
     }
 
     public void setRow(Scanner scanner){
-        this.row = scanner.nextInt();
+        this.row = scanner.nextInt() - 1;
     }
     public int getRow(){
         return row;
@@ -66,15 +81,15 @@ public class Main {
     }
 
     public void setCol(Scanner scanner) {
-        this.col = scanner.nextInt();
+        this.col = scanner.nextInt() - 1;
     }
 
 
     public void resetBoard() {
         // fills board with ' - ' to show that it is empty
 
-        for (int i = 0; i < boardLength; i++) {
-            for (int o = 0; o < boardLength; o++) {
+        for (int i = 0; i < board.length; i++) {
+            for (int o = 0; o < board.length; o++) {
                 board[i][o] = '-';
             }
         }
@@ -93,6 +108,7 @@ public class Main {
     }
 
     public void setBoard(int row , int col){
+        // using user inputs to update the tictactoe board
         if (isPlayer1 == true){
             board[row][col] = 'x';
         }else {
@@ -101,7 +117,12 @@ public class Main {
 
     }
 
+    public char getBoard(int row, int col){
+        return board[row][col];
+    }
+
     public String whoTurn(){
+        // checks to see whos turn it is
         if (isPlayer1 == true){
             return "player ones turn";
         } else {
